7098a053ff242bd90b2f26c55cb8c9abe30193de
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-01-30 00:48:35 +0100

    ajout des dossiers

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  document.querySelector("#formulary").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.body.style.overflow = "hidden";
+    document.querySelector(".modal").classList.remove("hidden");
+    console.log("ok");
+  });
+
+  document.querySelector("#close").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.body.style.overflow = "auto";
+    document.querySelector(".modal").classList.add("hidden");
+  });
+
+  document
+    .querySelector("#contact")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+      };
+
+      const response = await axios.post(
+        "site--formulaire-back--dl97zr2bcjvg.code.run/form",
+        data
+      );
+      console.log(response.data);
+    });
+});
